23a3628b5df91eb7c37e4c77d3a4ad98ae559c91
Sarabac <lucas.boutarfa@gmail.com>
2019-07-17 14:45:22 +0200

    Update Analyse_NDVIPhases.R

--- Analyse_NDVIPhases.R
+++ Analyse_NDVIPhases.R
@@ -1,2 +1,34 @@
 W.DIR = "L:/Lucas/phenology/ParaPhase"
 setwd(W.DIR)
+source("Variables.R")
+library(tidyverse)
+library(DBI)
+
+
+conn = dbConnect(RSQLite::SQLite(), paste(OUT.SQLITE, "sqlite", sep="."))
+
+NDVI = tbl(conn, "NDVI") %>% 
+  select(Pixel_ID, Date, NDVI) %>% 
+  collect()
+Phase = tbl(conn, "DOY") %>% 
+  select(Pixel_ID,Crop,Date,P) %>% 
+  filter(Crop=="201") %>% 
+  collect()
+dbDisconnect(conn)
+
+g = full_join(NDVI, Phase, by=c("Pixel_ID", "Date"))
+  
+
+DOY = Phase %>% 
+  arrange(Pixel_ID,Crop,Date,P) %>% 
+  group_by(Pixel_ID,Crop) %>% 
+  mutate(next_P = c(P[-1], 0), next_Date = c(Date[-1],0)) %>%
+  filter(row_number()!=n()) %>% 
+  ungroup() %>% 
+  mutate(period = as.factor(paste(P,next_P, sep="_")))
+
+test = DOY %>% 
+  group_by(Pixel_ID, period) %>% 
+  expand(Date=if_else(
+    
+  ))
